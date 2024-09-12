fd694969b92b6bc7e250db88d3dfdcf038ee62df
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-09-12 15:13:28 +0530

    Module 6 Lecture 6 12-9

--- Tailwind/installation/tailwind.config.js
+++ Tailwind/installation/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        divyaraj: '#23a9f2',
+        komal: '#da4033',
+      },
+      boxShadow: {
+        'bx-15':
+          'rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px',
+
+        'bx-30':
+          'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
+      },
+    },
   },
   plugins: [],
 };
